d082620976f065bdc74173c327a7403bf1652727
Andrew Bastien <volundmush@gmail.com>
2020-01-15 05:34:39 -0500

    Splitting Athanor into plugins and creating initial plugins!

--- athanor_job/__init__.py
+++ athanor_job/__init__.py
@@ -1,1 +1,9 @@
 INSTALLED_APPS = ["athanor_job"]
+
+GLOBAL_SCRIPTS = dict()
+
+GLOBAL_SCRIPTS['job'] = {
+    'typeclass': 'athanor_job.controllers.AthanorJobManager',
+    'repeats': -1, 'interval': 60, 'desc': 'Job API for Job System',
+    'locks': "admin:perm(Admin)",
+}
